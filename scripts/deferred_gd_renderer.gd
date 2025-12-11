43641e282da0ad63c3edff39b6acb6d0b4ed5fa2
jponzo <jeremy.ponzo@gmail.com>
2025-12-11 07:15:11 +0100

    WIP impl geometry pass (fix types)

--- scripts/deferred_gd_renderer.gd
+++ scripts/deferred_gd_renderer.gd
@@ -1,8 +1,8 @@
 extends ORC_RendererBase
 class_name ORC_DeferredGDRenderer
 
-var current_cam_data : ORC_PotatoGD_CameraData
-var opaque_surfaces_data : Array[ORC_PotatoGD_SurfaceData]
+var current_cam_data : ORC_DeferredGD_CameraData
+var opaque_surfaces_data : Array[ORC_DeferredGD_SurfaceData]
 
 func setup_override() -> void:
 	pass
@@ -10,14 +10,14 @@ func setup_override() -> void:
 func pre_render_override() -> void:
 	super_pre_render()
 	var cameras_data : Array[ORC_ProxyData] = scene_proxy.fetch_queue_data("cameras")
-	for camera_data : ORC_PotatoGD_CameraData in cameras_data:
+	for camera_data : ORC_DeferredGD_CameraData in cameras_data:
 		if camera_data.proxy_object.node.current:
 			current_cam_data = camera_data
 			break
 	
 	var non_casted_surfaces_data : Array = scene_proxy.fetch_queue_data("opaque_surfaces")
 	opaque_surfaces_data.clear()
-	for surface_data : ORC_PotatoGD_SurfaceData in non_casted_surfaces_data:
+	for surface_data : ORC_DeferredGD_SurfaceData in non_casted_surfaces_data:
 		opaque_surfaces_data.append(surface_data)
 	
 func render_override() -> void:
