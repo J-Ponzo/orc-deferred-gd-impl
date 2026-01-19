fd7fd8931733b50d00a4ebe3920785c38e798df6
JPonzo <jponzo@wanadev.fr>
2026-01-19 13:47:02 +0100

    move safe_rid to c++

--- scripts/rid_helpers/index_array_rid.gd
+++
@@ -1,6 +0,0 @@
-# TODO : put in open_render_craft if we keep it
-extends ORC_SafeRID
-class_name ORC_IndexArrayRID
-
-func is_valid() -> bool:
-	return rid.is_valid()
--- scripts/rid_helpers/texture_rid.gd
+++
@@ -1,7 +0,0 @@
-# TODO : put in open_render_craft if we keep it
-extends ORC_SafeRID
-class_name ORC_TextureRID
-
-func is_valid() -> bool:
-	# TODO check if we needs ORC_RDHelper.get_rd().texture_is_valid(rid).
-	return rid.is_valid()
